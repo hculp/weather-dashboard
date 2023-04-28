00a73614b0e278fb4f4de42e0b4d11cc612b59cf
Houston Culpepper <howacul@gmail.com>
2023-04-28 19:47:46 -0400

    added css normalize to reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 const apiKey = '8bd027d381a69ec1485e04e18a1d1f6f';
 const apiEndpoint = 'api.openweathermap.org/';
 var currentDay = dayjs().format('M/DD/YYYY');
+var searchBtn = document.querySelector('.btn.btn-primary.search');
+var searchHistory = [];
 
 console.log(currentDay);
 
+function currentWeather(city) {
+    var apiURL = apiEndpoint + 'geo/1.0/direct?q=' + city 
+}
+
+var findCity = function (event) {
+    event.preventDefault();
+    var city = $('#city-input').val().trim();
+    
+    if (!searchHistory.includes(city)) {
+        searchHistory.push(city);
+        var searchedCity = $(`
+        <li class="list-group-item">${city}</li>
+        `);
+         $("#searchHistory").append(searchedCity);
+    }
+}
+
+searchBtn.addEventListener('click',findCity);
